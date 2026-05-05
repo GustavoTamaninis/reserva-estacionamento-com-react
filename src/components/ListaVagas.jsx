2da814d8a498c3b8e0cdb797f7c2ce7b3034c1d2
GustavoTamaninis <194152443+GustavoTamaninis@users.noreply.github.com>
2026-05-05 20:09:57 -0300

    feat: listagem de vagas

--- src/components/ListaVagas.jsx
+++ src/components/ListaVagas.jsx
@@ -1,8 +1,47 @@
+import { useEffect, useState } from 'react'
+// import Formulario from "./Formulario";
 function ListarVagas(){
+    const [vagas, setVagas] = useState([
+        {"vaga": "01", "disponibilidade": "Disponível"},
+        {"vaga": "02", "disponibilidade": "Disponível"},
+        {"vaga": "03", "disponibilidade": "Disponível"},
+        {"vaga": "04", "disponibilidade": "Disponível"},
+        {"vaga": "05", "disponibilidade": "Disponível"},
+        {"vaga": "06", "disponibilidade": "Disponível"},
+        {"vaga": "07", "disponibilidade": "Disponível"},
+        {"vaga": "08", "disponibilidade": "Disponível"},
+        {"vaga": "09", "disponibilidade": "Disponível"},
+        {"vaga": "10", "disponibilidade": "Disponível"}
+    ]);
+
+    useEffect(() => {
+        const vagasArmazenadas = JSON.parse(localStorage.getItem("vagas"));
+        if(Array.isArray(vagasArmazenadas   )){
+            setVagas(vagasArmazenadas);
+        }else{
+            localStorage.setItem("vagas", JSON.stringify(vagas));
+        } 
+    }, []);
 
     return(
         <section>
             <h1>Vagas</h1>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Vaga</th>
+                        <th>Disponibilidade</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {vagas.map((vaga, index) => (
+                        <tr key={index}>
+                            <th>{vaga.vaga}</th>
+                            <th>{vaga.disponibilidade}</th>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
         </section>
     )
 }
